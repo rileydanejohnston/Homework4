f8b7e93f9b9da2d85d48d77306c8f115963a6038
Riley Johnston <rileydanejohnston@gmail.com>
2021-11-04 10:00:44 -0500

    Initial Commit

---
+++ Homework4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Homework4
+//
+//  Created by Riley Johnston on 11/4/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
